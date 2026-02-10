56e5494ab1c59fadb4594a126cb40a0ea1c3e7f2
Juan <juanezcaa@gmail.com>
2026-02-10 14:02:56 -0600

    config site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
+  site: "https://hubsho.com",
   integrations: [tailwind()],
 });
